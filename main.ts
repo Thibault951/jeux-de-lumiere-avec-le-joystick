d3f37f484987d45ca120795aa5adf33f741cbf69
Thibault951 <132680158+Thibault951@users.noreply.github.com>
2023-06-02 17:16:40 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,52 @@
-let strip: neopixel.Strip = null
-let strip2: neopixel.Strip = null
 function intensiter_en_fonction_du_joystick () {
-    if (BitMaker.read_Ain(AnalogPort.P0) > 225 && BitMaker.read_Ain(AnalogPort.P0) > 250) {
+    if (BitMaker.read_Ain(AnalogPort.P0) > 225 && BitMaker.read_Ain(AnalogPort.P0) <= 300) {
+        strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
         strip.setBrightness(100)
-    } else if (BitMaker.read_Ain(AnalogPort.P0) > 250 && BitMaker.read_Ain(AnalogPort.P0) > 320) {
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 300 && BitMaker.read_Ain(AnalogPort.P0) <= 360) {
+        strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+        strip.setBrightness(60)
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 360 && BitMaker.read_Ain(AnalogPort.P0) < 450) {
+        strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+        strip.setBrightness(30)
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setBrightness(0)
+    }
+}
+function intensiter_2_en_fonction_du_joystick2 () {
+    if (BitMaker.read_Ain(AnalogPort.P0) > 510 && BitMaker.read_Ain(AnalogPort.P0) > 550) {
+        strip.setBrightness(100)
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 550 && BitMaker.read_Ain(AnalogPort.P0) > 620) {
         strip.setBrightness(75)
-    } else if (BitMaker.read_Ain(AnalogPort.P0) > 320 && BitMaker.read_Ain(AnalogPort.P0) > 450) {
+    } else if (BitMaker.read_Ain(AnalogPort.P0) > 620 && BitMaker.read_Ain(AnalogPort.P0) > 700) {
         strip.setBrightness(50)
     } else {
         strip.setBrightness(0)
     }
 }
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+strip.show()
 basic.forever(function () {
-    if (BitMaker.read_Ain(AnalogPort.P0) > 225 && BitMaker.read_Ain(AnalogPort.P0) < 430) {
+    if (BitMaker.read_Ain(AnalogPort.P0) > 225 && BitMaker.read_Ain(AnalogPort.P0) < 450) {
         strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
-        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
         intensiter_en_fonction_du_joystick()
         strip.show()
     } else {
-        strip2 = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
-        strip2.setBrightness(0)
-        strip2.show()
+        strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+        strip.setBrightness(0)
+        strip.show()
+    }
+    if (BitMaker.read_Ain(AnalogPort.P0) > 460 && BitMaker.read_Ain(AnalogPort.P0) < 700) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        intensiter_2_en_fonction_du_joystick2()
+        strip.show()
+    } else {
+        strip.setBrightness(0)
+        strip.show()
     }
 })
